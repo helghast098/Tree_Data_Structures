84245e1aed7f93405742f7d3f3d26248b237d952
fcharles <fcharles@ucsc.edu>
2026-03-23 02:53:19 -0700

    test(binary): implement test for dfs
    How the test data is formated for the dfs test is that the first number after the test case is the type of dfs: 0=preorder, 1=inorder, 2=post order. After the type, then it's the expected sequence returned from the type of dfs. The next data after the | is the tree initializer string.

--- test/binary/test.cpp
+++ test/binary/test.cpp
@@ -10,7 +10,7 @@
 
 const std::string data_dir = DATA_DIR;
 
-bool data_parser( const std::string &s,  int &test_case, int &expected_result, std::string &nodes_str ) {
+bool data_parser( const std::string &s,  int &test_case, std::string &expected_result, std::string &nodes_str ) {
     // getting test number
     size_t test_num_end =  s.find( ':' );
     if ( test_num_end == std::string::npos ) {
@@ -18,7 +18,7 @@ bool data_parser( const std::string &s,  int &test_case, int &expected_result, s
     }
 
     test_case = std::stoi( s.substr( 0, test_num_end ) );
-    std::cerr << "Test Case: " << test_case << std::endl;
+    std::cerr << "============== Test Case: " << test_case <<  " ============== " << std::endl;
 
     // getting test expected result
     size_t  test_expected_start = test_num_end + 2;
@@ -27,9 +27,8 @@ bool data_parser( const std::string &s,  int &test_case, int &expected_result, s
         return false;
     }
 
-    expected_result = std::stoi( s.substr( test_expected_start, test_expected_end - test_expected_start ) );
+    expected_result = s.substr( test_expected_start, test_expected_end - test_expected_start );
     std::cerr << "Expected: " << expected_result << std::endl;
-
 
     // getting test_data
     size_t node_data_start = test_expected_end + 2;
@@ -41,16 +40,27 @@ bool data_parser( const std::string &s,  int &test_case, int &expected_result, s
     return true;
 }
 
+std::vector<int> string_to_vec( const std::string& s ) {
+    std::vector<int> vec;
+    std::istringstream s_stream(s);
+    std::string val;
 
-TEST(BinaryTree, IsFullBinary) {
-    std::ifstream input(data_dir + "/full_binary.txt");
+    while (std::getline( s_stream, val, ' ' ) ) {
+        vec.push_back( std::stoi( val ) );
+    }
+
+    return vec;
+}
+
+TEST( BinaryTree, IsFullBinary ) {
+    std::ifstream input( data_dir + "/full_binary.txt" );
     ASSERT_TRUE( input.good() ) << strerror( errno );
     std::string line;
 
     int test_case = 0;
-    int expected_result = 0;
+    std::string expected_result;
     std::string nodes_str;
-    while(std::getline(input, line)) {
+    while( std::getline( input, line ) ) {
         if ( !data_parser( line, test_case, expected_result, nodes_str ) ) {
             continue;
         }
@@ -59,8 +69,8 @@ TEST(BinaryTree, IsFullBinary) {
 
         bool got = tree.is_full_binary();
         std::cerr << "Got: " << got << std::endl;
+        ASSERT_EQ( std::stoi( expected_result ), got );
 
-        ASSERT_EQ( expected_result, got );
         nodes_str = "";
 
         std::cerr << "\n\n";
@@ -68,15 +78,15 @@ TEST(BinaryTree, IsFullBinary) {
     input.close();
 }
 
-TEST(BinaryTree, IsBalanced) {
-    std::ifstream input(data_dir + "/balanced.txt");
+TEST( BinaryTree, IsBalanced ) {
+    std::ifstream input( data_dir + "/balanced.txt" );
     ASSERT_TRUE( input.good() ) << strerror( errno );
     std::string line;
 
     int test_case = 0;
-    int expected_result = 0;
+    std::string expected_result;
     std::string nodes_str;
-    while(std::getline(input, line)) {
+    while( std::getline( input, line ) ) {
         if ( !data_parser( line, test_case, expected_result, nodes_str ) ) {
             continue;
         }
@@ -86,10 +96,40 @@ TEST(BinaryTree, IsBalanced) {
         bool got = tree.is_balanced();
         std::cerr << "Got: " << got << std::endl;
 
-        ASSERT_EQ( expected_result, got );
+        ASSERT_EQ( std::stoi( expected_result ), got );
         nodes_str = "";
 
         std::cerr << "\n\n";
     }
     input.close();
+}
+
+TEST( BinaryTree, DFS ) {
+    std::ifstream input(data_dir + "/dfs.txt");
+    ASSERT_TRUE( input.good() ) << strerror( errno );
+    std::string line;
+
+    int test_case = 0;
+    std::string expected_result;
+    std::string nodes_str;
+    while( std::getline( input, line ) ) {
+        if ( !data_parser( line, test_case, expected_result, nodes_str ) ) {
+            continue;
+        }
+
+        DFS_TYPE type = ( expected_result[0] == '0' ) ? DFS_TYPE::PREORDER : ( expected_result[0] == '1' ) ? DFS_TYPE::INORDER : DFS_TYPE::POSTORDER;
+        BinaryTree tree( nodes_str );
+
+        std::vector<int> expected_dfs = string_to_vec( expected_result.substr( 2 ) ); // 2 is used to skip to first number in expected list
+        std::vector<int> got_dsf = tree.dfs( type );
+
+        ASSERT_EQ( expected_dfs, got_dsf );
+
+        nodes_str = "";
+
+        std::cerr << "\n\n";
+    }
+    input.close();
+
+    
 }
